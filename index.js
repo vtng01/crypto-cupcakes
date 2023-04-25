dbe42f5737e4fcf54ae28a5d39c467d4e8f75548
Van Nguyen <vanthoai@fb.com>
2023-04-25 15:24:57 +0100

    Added some data extraction using oidc

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-require('dotenv').config('.env');
-const cors = require('cors');
-const express = require('express');
+require("dotenv").config(".env");
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const { PORT = 3000 } = process.env;
+const morgan = require("morgan");
+const { auth } = require("express-openid-connect");
+const {
+  AUTH0_SECRET,
+  AUTH0_BASE_URL,
+  AUTH0_CLIENT_ID,
+  AUTH0_ISSUER_BASE_URL,
+  PORT = 3000,
+} = process.env;
 // TODO - require express-openid-connect and destructure auth from it
 
-const { User, Cupcake } = require('./db');
+const { User, Cupcake } = require("./db");
+const config = {
+  authRequired: true,
+  auth0Logout: true,
+  secret: AUTH0_SECRET,
+  baseURL: AUTH0_BASE_URL,
+  clientID: AUTH0_CLIENT_ID,
+  issuerBaseURL: AUTH0_ISSUER_BASE_URL,
+};
 
 // middleware
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 /* *********** YOUR CODE HERE *********** */
 // follow the module instructions: destructure config environment variables from process.env
 // follow the docs:
-  // define the config object
-  // attach Auth0 OIDC auth router
-  // create a GET / route handler that sends back Logged in or Logged out
+// define the config object
+// attach Auth0 OIDC auth router
+// create a GET / route handler that sends back Logged in or Logged out
 
-app.get('/cupcakes', async (req, res, next) => {
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+app.get("/", (req, res) => {
+  const user = req.oidc.user;
+  if (user) {
+    console.log(user);
+    res.send(
+      `
+        <h1>Welcome ${user.given_name}</h1>
+        <p>${user.email}</p>
+
+      `
+    );
+  }
+});
+app.get("/cupcakes", async (req, res, next) => {
   try {
     const cupcakes = await Cupcake.findAll();
     res.send(cupcakes);
@@ -33,12 +64,11 @@ app.get('/cupcakes', async (req, res, next) => {
 
 // error handling middleware
 app.use((error, req, res, next) => {
-  console.error('SERVER ERROR: ', error);
-  if(res.statusCode < 400) res.status(500);
-  res.send({error: error.message, name: error.name, message: error.message});
+  console.error("SERVER ERROR: ", error);
+  if (res.statusCode < 400) res.status(500);
+  res.send({ error: error.message, name: error.name, message: error.message });
 });
 
 app.listen(PORT, () => {
   console.log(`Cupcakes are ready at http://localhost:${PORT}`);
 });
-
